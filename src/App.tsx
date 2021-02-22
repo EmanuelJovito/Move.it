39b924917a2d93ae5ec287d05a05eb4bd269fe70
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-02-22 11:58:25 -0300

    feat: Criando Experience bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import { ExperienceBar } from './components/ExperienceBar'
 import './styles/global.css'
 
 function App() {
   return (
-    <h1>Hello NLW</h1>
+    <div className="container">
+      <ExperienceBar />
+    </div>
   );
 }
 
