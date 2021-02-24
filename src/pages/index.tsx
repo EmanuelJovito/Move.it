e1c8faaf9c833e9075cf05d6e1e8025135235f58
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-02-24 10:03:31 -0300

    feat: Criando challenge box

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+import Head from 'next/head' 
+
 import { CompletedChallenges } from '../components/CompletedChallenges'
 import { Countdown } from '../components/Countdown'
 import { ExperienceBar } from '../components/ExperienceBar'
 import { Profile } from '../components/Profile'
+import { ChallengeBox } from '../components/ChallengeBox'
 
-import Head from 'next/head' 
 
 import styles from '../styles/pages/Home.module.css'
 
@@ -22,7 +24,9 @@ export default function Home() {
           <CompletedChallenges />
           <Countdown />
         </div>
-        <div></div>
+        <div>
+          <ChallengeBox />
+        </div>
       </section>
     </div>
   )
