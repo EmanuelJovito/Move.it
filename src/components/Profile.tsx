8fa6af32b2da30ec5bcfebe2621edc5754b47fc4
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-02-25 09:40:43 -0300

    feat: Criando estrutura de leveling

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react'
+import { ChallengeContext } from '../contexts/ChallengeContext'
 import styles from '../styles/components/Profile.module.css'
 
 export function Profile() {
+  const { level } = useContext(ChallengeContext)
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/EmanuelJovito.png" alt="Emanuel Jovito"/>
@@ -8,7 +12,7 @@ export function Profile() {
         <strong>Emanuel Jovito</strong>
         <p>
           <img src="icons/level.svg" alt="Level"/>
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ import { ChallengesProvider } from '../contexts/ChallengeContext'
 function MyApp({ Component, pageProps }) {
   return (
     <ChallengesProvider>
-      <Component {...pageProps} />
+        <Component {...pageProps} />
     </ChallengesProvider>
   )
 }
