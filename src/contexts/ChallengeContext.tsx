3370fbdd5859e495862deb399bfb366d6378e56d
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-02-26 09:52:07 -0300

    feat: Salvando informções no cookie

--- src/contexts/ChallengeContext.tsx
+++ src/contexts/ChallengeContext.tsx
@@ -1,5 +1,5 @@
 import { createContext, ReactNode, useEffect, useState } from "react";
-
+import Cookies from 'js-cookie'
 import challenges from '../../challenges.json'
 
 interface Challenge {
@@ -22,14 +22,22 @@ interface ChallengesContextData {
 
 interface ChallengesProviderProps {
   children: ReactNode
+
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
 }
+
 
 export const ChallengeContext = createContext({} as ChallengesContextData)
 
-export function ChallengesProvider({ children }: ChallengesProviderProps) {
-  const [level, setLevel] = useState(1)
-  const [currentExperience, setCurrentExperience] = useState(0)
-  const [challengesCompleted, setChallengesCompleted] = useState(0)
+export function ChallengesProvider({ 
+  children, 
+  ...rest
+}: ChallengesProviderProps) {
+  const [level, setLevel] = useState(rest.level ?? 1)
+  const [currentExperience, setCurrentExperience] = useState(rest.currentExperience ?? 0)
+  const [challengesCompleted, setChallengesCompleted] = useState(rest.challengesCompleted ?? 0)
 
   const [activeChallenge, setActiveChallenge] = useState(null)
 
@@ -38,6 +46,12 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
   useEffect(() => {
     Notification.requestPermission()
   }, [])
+
+  useEffect(() => {
+    Cookies.set('level', String(level))
+    Cookies.set('challengesCompleted', String(challengesCompleted))
+    Cookies.set('currentExperience', String(currentExperience))
+  }, [level, challengesCompleted, currentExperience])
 
   function levelUp() {
     setLevel(level + 1)
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,9 +4,7 @@ import { ChallengesProvider } from '../contexts/ChallengeContext'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChallengesProvider>
         <Component {...pageProps} />
-    </ChallengesProvider>
   )
 }
 
